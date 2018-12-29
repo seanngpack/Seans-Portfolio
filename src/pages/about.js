1b4bc7d35274142a738b2d929e678a0d87c5123a
sean ng pack <35155526+seanngpack@users.noreply.github.com>
2018-12-29 14:02:34 -0500

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -8,13 +8,44 @@ import CarouselLol from '../container/CarouselPage/index'
 
 class BlogPostTemplate extends React.Component {
   render() {
-      console.log(this.props.data)
+    console.log(this.props.data)
     return (
       <Layout>
         <div className={styles.container}>
-          <Intro title={'Sean Ng Pack'} excerpt={'whats up'} logo={this.props.data.profile.childImageSharp.fluid} />
-          <div className={styles.page} />
-          the bulk of the content goes here lol, this is the page
+          <Intro
+            title={'Sean Ng Pack'}
+            excerpt={'whats up'}
+            logo={this.props.data.profile.childImageSharp.fluid}
+          />
+          <div className={styles.page}>
+            <p>
+              I really do it all--fashion, photography, design, engineering,
+              computer science, writing, tennis, pool, piano and a vast array of
+              esoteric hobbies. I fail fast and learn fast to climb to the top
+              1% of each one of my pursuits.
+            </p>
+            I like giving back to communities, I'm very active and well-known in
+            several prominent communities for fashion, photography, pool. I lead
+            discussions and provide insight that has never been discussed
+            before.
+            <p />
+            <p>
+              In addition to participating in communities, I like creating new
+              things. In High school I co-founded the MHS Recycling Club which
+              grew to be the school's largest organization. At Northeastern I
+              co-founded the NU Pool Club and now head the school's Pool Team. I
+              also created Lengjai, an Asian-centric fashion/culture news
+              platform to empower Asian-Americans and increase representation in
+              the media.
+            </p>
+            <p>
+              I am a life-long learner and love combining my passions into
+              awesome creations. I think my strong mechanical engineering
+              knowledge, computer science skills, and design intuition are a
+              deadly combo for developing projects that are unlike any other.
+            </p>
+            <p>TLDR; I wish there were more hours in a day.</p>
+          </div>
         </div>
       </Layout>
     )
@@ -34,7 +65,7 @@ export const pageQuery = graphql`
     profile: file(relativePath: { eq: "profile-pic.jpg" }) {
       childImageSharp {
         fluid(maxWidth: 1000, maxHeight: 1000, cropFocus: CENTER) {
-            ...GatsbyImageSharpFluid
+          ...GatsbyImageSharpFluid
         }
       }
     }
